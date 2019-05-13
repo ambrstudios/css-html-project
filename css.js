e31f94754c2a518b989e11c9c78fa4952d787971
Chiq2045 <charles.oroko@outlook.com>
2019-05-13 19:46:01 +0000

    small changes

--- css.js
+++ css.js
@@ -1,8 +1,14 @@
 const Koa = require('koa');
 const serve = require('koa-static');
+// const cacheControl = require('koa-cache-control');
 
 const css = new Koa();
 
+// css.use(
+//   cacheControl({
+//     maxAge: 3600,
+//   }),
+// );
 css.use(serve(`${__dirname}/src`));
 css.listen(1340);
 
